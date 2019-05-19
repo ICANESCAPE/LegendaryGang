0a046bdd030ff5097a224c47007d58e2584f8202
Alchemy <2457043027@qq.com>
2019-05-19 17:13:06 +0800

    [+]将UnionSkills类标记为Data类

--- src/main/java/org/sct/legendgang/dto/UnionSkills.java
+++ src/main/java/org/sct/legendgang/dto/UnionSkills.java
@@ -1,9 +1,15 @@
 package org.sct.legendgang.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author SCT_Alchemy
  * @date 2019-05-17 19:58
  */
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UnionSkills {
 }
